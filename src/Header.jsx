2a00fa9771c871a53815e0203af04dde1addf9d9
Tejas Pattar <tejaspattar03@gmail.com>
2023-08-07 19:16:35 +0530

    first commit

---
+++ src/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BsPencilSquare } from "react-icons/bs";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        {" "}
+        <BsPencilSquare />  Notes
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/Note.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { AiOutlineDelete } from "react-icons/ai";
+
+
+function Note(props) {
+  function handleClick() {
+    props.onDelete(props.id);
+  }
+
+  return (
+    <div className="note">
+      <h1>{props.title}</h1>
+      <p>{props.content}</p>
+      <button onClick={handleClick}><AiOutlineDelete /></button>
+    </div>
+  );
+}
+
+export default Note;
